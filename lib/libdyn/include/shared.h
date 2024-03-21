3b8d8dcf585099dcfc4acc23d5fe3367bfa5c972
AugustoCabrera <augustogabrielcabrera@mi.unc.edu.ar>
2024-03-21 10:36:57 -0300

    Agrego y linkeo librerias

---
+++ lib/libdyn/include/shared.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * 
+ *  shared_lib_function
+ * 
+ * This function is defined in the shared library and is program.
+ * 
+ * @param msg: The message to be printed.
+ * @return The message size.
+ * 
+*/
+
+int shared_lib_function(char* msg);
